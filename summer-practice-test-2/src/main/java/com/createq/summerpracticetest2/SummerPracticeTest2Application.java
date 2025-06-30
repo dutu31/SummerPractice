576930f40adbd52387f216b64465dd0fb6e01808
Eduard-Ionut Rusu <eduard-ionut.rusu@student.tuiasi.ro>
2025-06-30 14:48:26 +0300

    adaugat

---
+++ summer-practice-test-2/src/main/java/com/createq/summerpracticetest2/SummerPracticeTest2Application.java
@@ -0,0 +1,13 @@
+package com.createq.summerpracticetest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummerPracticeTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummerPracticeTest2Application.class, args);
+    }
+
+}
